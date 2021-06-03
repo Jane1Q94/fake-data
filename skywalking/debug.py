5685590e94eb97c2e07aaea185a047ff1223dd17
杨基建 <jijian.yang@eoitek.com>
2021-06-03 13:01:28 +0800

    继续完善 skywalking

--- skywalking/debug.py
+++ skywalking/debug.py
@@ -1,14 +1,93 @@
+import time
+
 import requests
+
+url = "http://127.0.0.1:5000/generate/config"
+payload = {
+}
+response = requests.post(url=url, json=payload)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/get/config"
+response = requests.get(url=url)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/generate/traces"
+response = requests.post(url=url, json=payload)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/get/traces"
+response = requests.get(url=url)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/get/speed"
+response = requests.get(url=url)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/stop"
+payload = {
+    "exp": "len(cls.traces)==10"
+}
+response = requests.post(url=url, json=payload)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/update/traces/del"
+payload = {}
+response = requests.post(url=url, json=payload)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/get/traces"
+response = requests.get(url=url)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/update/traces/add"
+payload = {}
+response = requests.post(url=url, json=payload)
+print(response.json())
+print("-" * 160)
+
+url = "http://127.0.0.1:5000/get/traces"
+response = requests.get(url=url)
+print(response.json())
+print("-" * 160)
 
 url = "http://127.0.0.1:5000/start"
 payload = {
-    "serverNum": 10,
-    "serverInsNum": 10,
-    "serverEndPointNum": 10,
-    "traceNum": 4
+    "speed": 100000
 }
 response = requests.post(url=url, json=payload)
-if response.status_code == 200:
-    print(response.json())
-else:
-    print("错误")
+print(response.json())
+print("-" * 160)
+
+# url = "http://127.0.0.1:5000/start"
+# payload = {}
+# response = requests.post(url=url, json=payload)
+# print(response.json())
+# print("-" * 160)
+
+time.sleep(4)
+
+url = "http://127.0.0.1:5000/get/speed"
+response = requests.get(url=url)
+print(response.json())
+print("-" * 160)
+
+time.sleep(4)
+url = "http://127.0.0.1:5000/stop"
+payload = {}
+response = requests.post(url=url, json=payload)
+print(response.json())
+print("-" * 160)
+
+# url = "http://127.0.0.1:5000/get/speed"
+# response = requests.get(url=url)
+# print(response.json())
+# print("-" * 160)
--- skywalking/run.py
+++ skywalking/run.py
@@ -1,89 +1,263 @@
+import json
+
 from flask import Flask, request
+from concurrent.futures import ThreadPoolExecutor
+from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
 import logging
 import faker
 import uuid
 import random
 import datetime
 import time
+import queue
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(levelname)s: %(message)s')
 
 app = Flask(__name__)
 faker = faker.Faker()
 
-# 数据从配置中生成，改变配置，即改变数据
-config = {}
 
-# 保存生成的 trace
-traces = []
+@app.route("/update/network", methods=["POST"])
+def update_network():
+    network = request.json.get("network")
+    if network:
+        GenerateData.network = network
+    return {
+        "network": GenerateData.network
+    }
 
-flag = True
+
+@app.route("/update/delay", methods=["POST"])
+def update_delay():
+    endpoint = request.json.get("endpoint")
+    delay = request.json.get("delay")
+    if endpoint and delay:
+        list_endpoint = endpoint.split("@")
+        GenerateData.config[list_endpoint[0]][list_endpoint[1]][list_endpoint[2]]["delay"] = delay
+        return {
+            "delay": GenerateData.config[list_endpoint[0]][list_endpoint[1]][list_endpoint[2]]["delay"]
+        }
+    return {
+        "警告": "没有做任何改变"
+    }
+
+
+@app.route("/update/error", methods=["POST"])
+def update_error():
+    endpoint = request.json.get("endpoint")
+    error = request.json.get("error")
+    if endpoint and error:
+        list_endpoint = endpoint.split("@")
+        GenerateData.config[list_endpoint[0]][list_endpoint[1]][list_endpoint[2]]["error"] = error
+        return {
+            "error": GenerateData.config[list_endpoint[0]][list_endpoint[1]][list_endpoint[2]]["error"]
+        }
+    return {
+        "警告": "没有做任何改变"
+    }
+
+
+@app.route("/update/traces/del", methods=["POST"])
+def update_traces_del():
+    trace = GenerateData.traces.pop(0)
+    GenerateData.del_traces.append(trace)
+    return {
+        "num": len(GenerateData.traces),
+        "traces": GenerateData.traces
+    }
+
+
+@app.route("/update/traces/add", methods=["POST"])
+def update_traces_add():
+    if GenerateData.del_traces:
+        trace = GenerateData.del_traces.pop(0)
+        GenerateData.traces.insert(0, trace)
+    else:
+        trace = []
+        servers = list(GenerateData.config.keys())  # 获取所有的服务
+        while servers:  # trace 的长度为所有的服务长度
+            server = random.choice(servers)  # 随机选择某个服务
+            servers.remove(server)  # 将该服务从服务列表中删除，表示这个服务节点已经执行过，不需要再次执行
+            ins = random.choice(list(GenerateData.config[server].keys()))  # 随机选取该服务下的某个实例
+            endpoint = random.choice(list(GenerateData.config[server][ins].keys()))  # 随机选取该实例下的某个端点
+            trace.append("%s@%s@%s" % (server, ins, endpoint))  # 服务@实例@端点 组装成一个 span
+        GenerateData.traces.append(trace)
+    return {
+        "num": len(GenerateData.traces),
+        "traces": GenerateData.traces
+    }
+
+
+@app.route("/output", methods=["POST"])
+def output():
+    address = request.json.get("address")
+    topic = request.json.get("topic")
+    GenerateData.pool.submit(GenerateData.output, address, topic)
+    return {
+        "res": "success"
+    }
+
+
+@app.route("/restart", methods=["POST"])
+def restart():
+    GenerateData.flag = True
+    return {
+        "res": "success"
+    }
 
 
 @app.route("/start", methods=["POST"])
 def start():
-    global config
-    global traces
-    server_num = request.json.get("serverNum")
-    server_ins_num = request.json.get("serverInsNum")
-    server_endpoint_num = request.json.get("serverEndPointNum")
-    trace_num = request.json.get("traceNum")
+    GenerateData.pool.submit(GenerateData.start)
+    return {
+        "res": "success"
+    }
+
+
+@app.route("/stop", methods=["POST"])
+def stop():
+    """
+    停止实时数据
+    """
+    exp = request.json.get("exp", "1==1")
+    GenerateData.pool.submit(GenerateData.stop, exp)
+    return {
+        "res": "success"
+    }
+
+
+@app.route("/generate/traces", methods=["POST"])
+def generate_traces():
+    GenerateData.traces = []
+    trace_num = request.json.get("traceNum", 10)  # 生成 X 个 trace
+    # 生成 X 个 trace 的过程
+    while trace_num:
+        trace = []
+        servers = list(GenerateData.config.keys())  # 获取所有的服务
+        while servers:  # trace 的长度为所有的服务长度
+            server = random.choice(servers)  # 随机选择某个服务
+            servers.remove(server)  # 将该服务从服务列表中删除，表示这个服务节点已经执行过，不需要再次执行
+            ins = random.choice(list(GenerateData.config[server].keys()))  # 随机选取该服务下的某个实例
+            endpoint = random.choice(list(GenerateData.config[server][ins].keys()))  # 随机选取该实例下的某个端点
+            trace.append("%s@%s@%s" % (server, ins, endpoint))  # 服务@实例@端点 组装成一个 span
+        GenerateData.traces.append(trace)
+        trace_num -= 1
+    logging.debug(GenerateData.traces)
+    return {
+        "num": len(GenerateData.traces),
+        "traces": GenerateData.traces
+    }
+
+
+@app.route("/get/traces", methods=["GET"])
+def get_traces():
+    return {
+        "num": len(GenerateData.traces),
+        "traces": GenerateData.traces
+    }
+
+
+@app.route("/get/config", methods=["GET"])
+def get_config():
+    return GenerateData.config
+
+
+@app.route("/generate/config", methods=["POST"])
+def generate_config():
+    """
+    开始数据模拟
+    """
+
+    server_num = request.json.get("serverNum", 4)  # 服务的数量
+    server_ins_num = request.json.get("serverInsNum", 4)  # 每个服务的实例的数量
+    server_endpoint_num = request.json.get("serverEndPointNum", 4)  # 每个实例端点的数量
     logging.debug({
         "serverNum": server_num,
         "serverIns": server_ins_num,
         "serverEndpoint": server_endpoint_num,
-        "traceNum": trace_num
     })
 
-    if not (server_num and server_ins_num and server_endpoint_num and trace_num):
-        return {
-            "错误": "缺少参数！(serverNum, serverIns, serverEndPoint, traceNum)"
-        }
-    config = {
+    GenerateData.config = {
         faker.bs(): {faker.ipv4(): {faker.uri_path(4): {"delay": [10, 150], "error": [100, 1]} for _ in
                                     range(int(server_endpoint_num))} for _ in range(int(server_ins_num))} for _ in
-        range(int(server_num))}
+        range(int(server_num))}  # 默认每个端点的执行时间在 10~150ms之间，错误率在 1%
 
-    while trace_num:
-        servers = list(config.keys())  # 获取所有的服务
-        trace = []
-        while servers:
-            server = random.choice(servers)  # 随机选择某个服务
-            servers.remove(server)
-            ins = random.choice(list(config[server].keys()))  # 获取该服务下的实例
-            endpoint = random.choice(list(config[server][ins].keys()))  # 获取该实例下的端点
-            trace.append("%s@%s@%s" % (server, ins, endpoint))  # 组装成 trace
-        traces.append(trace)
-        trace_num -= 1
-    logging.debug(traces)
-    GenerateData.start()
-    return {"res": "success"}
+    return GenerateData.config
+
+
+@app.route("/get/speed", methods=["GET"])
+def get_speed():
+    start_num = GenerateData.num
+    time.sleep(1)
+    end_num = GenerateData.num
+    return {
+        "speed": end_num - start_num
+    }
+
+
+@app.route("/update/speed", methods=["POST"])
+def update_speed():
+    speed = int(request.json.get("speed", 1000))
+    GenerateData.queue_data.maxsize = speed
+    return {
+        "speed": GenerateData.queue_data.maxsize
+    }
 
 
 class GenerateData:
+    flag = True  # 控制实时数据的启停
+    config = {}  # 数据从配置中生成，改变配置，即改变数据
+    traces = []  # 保存生成的 trace
+    del_traces = []  # network delay
+    network = [100, 150]  # 网络延迟
+    pool = ThreadPoolExecutor()  # 线程池
+    num = 0  # 已产生的数据总量
+    queue_data = queue.Queue(maxsize=1000)
+
     @classmethod
-    def timestamp(cls):
-        now = datetime.datetime.now()
-        t = now.timetuple()
-        timestamp = int(time.mktime(t) * 1000.0 + now.microsecond / 1000.0)
+    def datetime2timestamp(cls, start_time):
+        t = start_time.timetuple()
+        timestamp = int(time.mktime(t) * 1000.0 + start_time.microsecond / 1000.0)
         return timestamp
 
     @classmethod
+    def output(cls, address, topic):
+        kafka = None
+        if address:
+            kafka = Kafka(address)
+
+        while cls.flag:
+            msg = cls.queue_data.get()
+            if kafka:
+                kafka.send(topic, msg)
+            else:
+                print(msg)
+
+    @classmethod
+    def stop(cls, exp):
+        while True:
+            if eval(exp):
+                cls.flag = False
+                break
+            time.sleep(0.001)
+
+    @classmethod
     def start(cls):
-        global flag
-        data = []
-        while flag:
-            traceId = uuid.uuid4().hex
-            trace = random.choice(traces)  # 随机选取一个 trace
-            startTime = None
-            for key, value in enumerate(trace):  # 封装 span 数据
+        while True:
+            trace = random.choice(cls.traces)  # 随机选取一个 trace
+            traceId = uuid.uuid4().hex  # 生成 traceId
+
+            data_time = 0
+            for key, value in enumerate(trace):  # 每个trace有 X 个span，这里组装每个span
                 d = {
                     "traceId": traceId
                 }
-                if not startTime:
-                    startTime = cls.timestamp()
+                if key == 0:  # 第一个 span 的开始时间
+                    data_time = cls.datetime2timestamp(datetime.datetime.now())
                 else:
-                    startTime += random.randint(100, 150)
+                    data_time += random.randint(*cls.network)  # 非第一个span的开始时间要在第一个span之后，经过一段时间的网络传输
+
                 value = value.split("@")
                 d["serviceName"] = value[0]
                 d["serviceInstanceName"] = value[0] + "@" + value[1]
@@ -91,15 +265,34 @@ class GenerateData:
                 d["parentSpanId"] = d["traceId"] + "_" + str(key - 1)
                 d["endpoint"] = value[2]
                 d["isError"] = \
-                    random.choices(["false", "true"], weights=config[value[0]][value[1]][value[2]]["error"], k=1)[0]
+                    random.choices(["false", "true"], weights=cls.config[value[0]][value[1]][value[2]]["error"], k=1)[0]
                 d["type"] = "whole"
-                d["startTime"] = startTime
-                d["endTime"] = d["startTime"] + random.randint(*config[value[0]][value[1]][value[2]]["delay"])
-                data.append(d)
-                if len(data) >= 1000:
-                    logging.debug(data)
-                    flag = False
-                    break
+                d["startTime"] = data_time
+                d["endTime"] = d["startTime"] + random.randint(*cls.config[value[0]][value[1]][value[2]]["delay"])
+                cls.num += 1
+                cls.queue_data.put(d)
+
+
+class Kafka:
+    def __init__(self, address):
+        self.client = KafkaAdminClient(bootstrap_servers=address, client_id='%s' % (uuid.uuid1()))
+
+        self.producer = KafkaProducer(bootstrap_servers=address,
+                                      value_serializer=lambda v: json.dumps(v, ensure_ascii=False).encode('utf-8'),
+                                      acks=0)
+
+    def create_topic(self, topic_name, partitions=1, replication=1):
+        try:
+            list_topic = [NewTopic(name=topic_name, num_partitions=partitions, replication_factor=replication)]
+            self.client.create_topics(new_topics=list_topic, validate_only=False)
+        except Exception as e:
+            logging.warning(e)
+
+    def send(self, topic_name, data):
+        try:
+            self.producer.send(topic_name, data)
+        except Exception as e:
+            logging.error('send data to kafka failed\n%s\n%s' % (e, data))
 
 
 if __name__ == '__main__':
